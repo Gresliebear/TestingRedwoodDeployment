1a7096d98cebe85fefe5c2d8a6894005b3a42e8a
Leslie Wubbel <lesliemwubbel@gmail.com>
2022-08-21 19:39:51 -0400

    pushingcode

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,15 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+import ArticlesCell from 'src/components/ArticlesCell'
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags title="Home" description="Home page" />
+
+
+      <ArticlesCell />
+    </>
+  )
+}
+
+export default HomePage
